88d07b1d0ec850e213fb45a4006127a12228d17e
24HP0880 <24hp0880@G305-CC0073.local>
2024-10-26 10:21:06 +0800

    upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { useState } from "react"
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+const Page = ()=>{
+
+  const [image, setImage] = useState("")
+  const [uploadedImage, setUploadedImage] = useState("")
+  
+  const handleImageValue = (e)=>{
+    const files = e.target.files[0]
+    setImage(files)
+
+  }
+
+  const uploadImage = async()=>{
+
+    if(!image){
+      alert("Please Select an Image Before Uploading")
+    }
+
+    const formData = new FormData();
+    formData.append(`file`, image)
+    formData.append('upload_preset', 'lsdifnsl')
+    formData.append('cloud_name', 'dvqlgaqsy')
+
+    try{
+      const get = await fetch("https://api.cloudinary.com/v1_1/dvqlgaqsy/image/upload",{
+        method:"POST",
+        body:formData,
+      })
+
+      const getJSON = await get.json()
+      console.log(getJSON)
+      setUploadedImage(getJSON.secure_url)
+      setImage('')
+    }catch (error){
+      console.log("ERROR")
+    }
+  }
+
+  console.log(image)
+  return(
+    <div>
+      <div>
+          <input type="file" onChange={(e)=>{handleImageValue(e)}} accept="image/*" />
+          <button onClick={()=>uploadImage()}>Upload Image</button>
+      </div>
+      {
+        uploadedImage? 
+        <div>
+          <h1>Uploaded Image</h1>
+          <img src= {uploadedImage} alt="Uploaded"/>
+        </div>:""
+      }
     </div>
-  );
+  )
 }
+
+export default Page;
